42538af079b646a0ae287f3026c75b69e4b5dbc6
Antonne Turner <antonneturner@gmail.com>
2021-01-28 14:55:31 -0500

    create schema file, create seed file, create database

---
+++ schema.sql
@@ -0,0 +1,18 @@
+Drop Database if exists Employee;
+Create DATABASE Employee;
+use employee;
+create table department (
+  id int not null auto_increment primary key,
+  name varchar(30)
+);
+create table role (
+  id int not null auto_increment primary key,
+  title varchar(30),
+  salary decimal (10, 2),
+  department_id int not null,
+  foreign key(department_id) references department(id)
+);
+Create table employee (
+  id int not null auto_increment primary key,
+  first_name varchar(30),
+  last_name varchar(30),
